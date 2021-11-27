4702dcae2c87cae5500e9203380515fd461efa3e
jiayue-xu <jiayue.xu@hotmail.com>
2021-11-27 03:35:15 -0500

    my solution for util lab exercise 1

--- user/xargs.c
+++ user/xargs.c
@@ -1,45 +1,69 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/fs.h"
 
-int main(int argc, char*argv[])
-{
-    int i, ac;
-    char c;
-    char *cmd;
-    char arg[32][128];
+char* readline() {
+    char* buf = malloc(100);
+    char* p = buf;
+    while(read(0, p, 1) != 0){
+        if(*p == '\n' || *p == '\0'){
+            *p = '\0';
+            return buf;
+        }
+        p++;
+    }
+    if(p != buf) return buf;
+    free(buf);
+    return 0;
+}
 
-    if(argc < 2){
-        exit(1);
-    }else{
-        for(int i=0;i<32;i++){
-            memset(arg[i], 0, 128);
-        }
-        cmd = argv[1];
-        for(i = 2; i < argc; i++){
-            strcpy(arg[i-1], argv[i]);
-        }
-        ac = i - 1;
-        while(read(0, &c, 1)==1){
-            if(c=='\n'){
-                arg[ac][0] = 0;
-                if(fork()){
-                    wait(0);
-                    for(int j=i-1;j<32;j++){
-                         memset(arg[j], 0, 128);
-                    }
-                    ac = i-1;
-                }else{
-                    exec(cmd, (char**)arg);
-                }
-            }else if(c==' '){
-                if(arg[ac][0]){
-                    ac++;
-                }
+int
+main(int argc, char *argv[]){
+    if(argc < 2) {
+        printf("Usage: xargs [command]\n");
+        exit(-1);
+    }
+    char* l;
+    argv++;
+    char* nargv[16];
+    char** pna = nargv;
+    char** pa = argv;
+    while(*pa != 0){
+        *pna = *pa;
+        pna++;
+        pa++;
+    }
+    while((l = readline()) != 0){
+        //printf("%s\n", l);
+        char* p = l;
+        char* buf = malloc(36);
+        char* bh = buf;
+        int nargc = argc - 1;
+        while(*p != 0){
+            if(*p == ' ' && buf != bh){
+                *bh = 0;
+                nargv[nargc] = buf;
+                buf = malloc(36);
+                bh = buf;
+                nargc++;
             }else{
-                memmove(arg[ac], &c, 1);
+                *bh = *p;
+                bh++;
             }
+            p++;
+        }
+        if(buf != bh){
+            nargv[nargc] = buf;
+            nargc++;
+        }
+        nargv[nargc] = 0;
+        free(l);
+        int pid = fork();
+        if(pid == 0){
+            // printf("%s %s\n", nargv[0], nargv[1]);
+            exec(nargv[0], nargv);
+        }else{
+            wait(0);
         }
     }
     exit(0);
